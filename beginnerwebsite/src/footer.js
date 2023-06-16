7bbcf8dd7ff693374da46bf58281b87aaf185fc8
Bobrekk <81027034+Bobrekk@users.noreply.github.com>
2023-06-16 23:18:33 +0300

    Add files via upload

---
+++ beginnerwebsite/src/footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./footer.css"
+import { FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
+
+export default function footer() {
+  return (
+    <footer>
+        <div className='container'>
+            <div className='social'>
+                <div className='soci'><FaGithub/></div>
+                <div className='soci'><FaInstagram/></div>
+                <div className='soci'><FaTwitter/></div>
+            </div>
+            <div className='copyright'>
+                Copyright &copy; 2023,Burak Korkut
+            </div>
+        </div>
+    </footer>
+  )
+}
---
+++ beginnerwebsite/src/main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./main.css"
+
+export default function main() {
+  return (
+    <div className='site-info'>
+        <div className='title'>
+            Burak
+        </div>
+        <div className='sub-title'>
+            Back-End Developer
+        </div>
+    </div>
+  )
+}
